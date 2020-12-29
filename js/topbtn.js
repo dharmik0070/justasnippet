fa10f2385f707eee71fe3070b33c50331e612dd9
Dharmik Shah <dharmik0070@gmail.com>
2020-12-29 23:15:58 +0530

    Added 'Go to top button' and path fixing

---
+++ js/topbtn.js
@@ -0,0 +1,16 @@
+var mybutton = document.getElementById("topBtn");
+
+window.onscroll = function() { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
